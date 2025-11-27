3d47ac57c68b05d50710d729d8f2c82a6da0c551
mhd7pf <realmoddoss@gmail.com>
2025-11-27 15:22:15 +0000

    Creation de la navbar et des liens Route pour acceder aux pages sur la navbar.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,34 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'home')]
+    public function indexAction(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
+
+    #[Route('/portfolio', name: 'portfolio')]
+    public function portfolio(): Response
+    {
+        return $this->render('autres/portfolio.html.twig');
+    }
+
+    #[Route('/formulaire', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('autres/formulaire.html.twig');
+    }
+
+    #[Route('/apropos', name: 'about')]
+    public function apropos(): Response
+    {
+        return $this->render('autres/apropos.html.twig');
+    }
+
+    #[Route('/cv', name: 'cv')]
+    public function cv(): Response
+    {
+        return $this->render('autres/cv.html.twig');
+    }
+
 }
